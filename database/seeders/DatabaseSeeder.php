fc02740d929f8af3a381a33cd3af6aeaaaad8da0
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-12-13 14:48:29 +0600

    user component is created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' =>'admin'.'@gmail.com',
+            'password' =>Hash::make('password'),
+            'email_verified_at' => now(),
+            'created_at'=>	now(),
+            'updated_at'=>now(),
+        ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -10,20 +10,20 @@ import { createApp } from 'vue';
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
  * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
+ * to use in your application's views. An user is included for you.
  */
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+import UserComponent from './components/UserComponent.vue';
+app.component('user-component', UserComponent);
 
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/UserComponent.vue -> <user-component></user-component>
  */
 
 // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
